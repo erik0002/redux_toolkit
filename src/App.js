d20baae1a34c8bd7df90b92983c75e7efa2adc42
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-01 14:25:17 +0300

    add fetch request to database

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import {useSelector} from "react-redux";
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {useEffect} from "react";
 
 function App() {
 
     const showCart = useSelector(state => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+
+    useEffect(() => {
+        fetch('https://redux-toolkit-1111b-default-rtdb.europe-west1.firebasedatabase.app/car.json', {
+            method: 'PUT',
+            body: JSON.stringify(cart),
+        })
+    }, [cart]);
+
     return (
         <Layout>
             {showCart && <Cart/>}
